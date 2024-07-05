e83c31eb7b71b7257bad9860924eab57ef3d785d
DELCROIX Victor <37382189+LeadCreep@users.noreply.github.com>
2024-07-05 17:12:13 +0200

    Player Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,36 +6,72 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Camera mainCamera;
     private GameManager manager;
     private InputAction moveAction;
     private InputAction lookAction;
+    private CharacterController characterController;
 
-    [SerializeField] private Camera camera;
+
+    private bool isMoving = false;
 
     [SerializeField] private float speed = 0.12f;
+    [SerializeField] private float mouseSensitivity = 1f;
+    [SerializeField] private float upDownLookRange = 80f;
     private Vector2 moveValue;
+    private Vector2 lookValue;
+    private Vector3 currentMovement;
+    private float mouseVerticalRotation = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
         manager = GameManager.GetInstance();
+        mainCamera = Camera.main;
+        characterController = GetComponent<CharacterController>();
         moveAction = manager.GetInputs.actions["Move"];
         lookAction = manager.GetInputs.actions["Look"];
+        manager.LockCursor(true);
     }
 
     private void HandleCamRotation()
     {
-        float mouseXRotation;
+        lookValue = lookAction.ReadValue<Vector2>();
+        float mouseXRotation = lookValue.x * mouseSensitivity;
+        transform.Rotate(0, mouseXRotation, 0);
+
+        mouseVerticalRotation -= lookValue.y * mouseSensitivity;
+        mouseVerticalRotation = Mathf.Clamp(mouseVerticalRotation, -upDownLookRange, upDownLookRange);
+        mainCamera.transform.localRotation = Quaternion.Euler(mouseVerticalRotation, 0, 0);
+    }
+
+    private void HandleMovement()
+    {
+        float verticalSpeed = moveValue.y * speed;
+        float horizontalSpeed = moveValue.x * speed;
+
+        Vector3 horizontalMovement = new Vector3(horizontalSpeed, 0, verticalSpeed);
+        horizontalMovement = transform.rotation * horizontalMovement;
+
+        // Gravity
+
+        currentMovement.x = horizontalMovement.x;
+        currentMovement.z = horizontalMovement.z;
+
+        characterController.Move(currentMovement * Time.deltaTime);
+
+        isMoving = verticalSpeed != 0 || horizontalSpeed != 0;
     }
 
     void Update()
     {
+        HandleCamRotation();
         moveValue = moveAction.ReadValue<Vector2>();
     }
 
     void FixedUpdate()
     {
-        gameObject.transform.position += new Vector3(moveValue.x, 0, moveValue.y) * speed;
-
+        HandleMovement();
     }
 }

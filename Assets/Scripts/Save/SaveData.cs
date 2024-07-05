a1e3ad806fe2e5562828e7d3386179a6d2f4f86c
BRISSY Axel <axelbrissy0804@gmail.com>
2024-07-05 17:17:51 +0200

    PlayerInput in GameManager

---
+++ Assets/Scripts/Save/SaveData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData : MonoBehaviour
+{
+
+}
+
+[System.Serializable]
+public class Data
+{
+    public List<string> notePadData;
+
+}
